b8db936cb3d9fa738bf7c5085b8c2fc851e4e08b
Aritro Saha <titanssuperior@gmail.com>
2023-06-18 00:49:52 +0530

    added mongoDB Connection

--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
+const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
 const app = express()
 
 const port = process.env.PORT || 5000;
 
+connectDb();
 app.use(express.json());
 app.use("/api/contacts",require("./routes/contactRoutes"));
 app.use(errorHandler)
